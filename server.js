df50047910aebe641bc99c66e0e765338804911b
Nahila-MouhibElidrissi <nhlmouhib@gmail.com>
2021-05-04 03:47:58 +0000

    nmefdq

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/<FOLDER_NAME>"));
+app.use(express.static(__dirname + "/dist/pfaFront"));
 
 app.get("/*", (req, res, next) => {
   res.sendFile(path.join(__dirname + "/dist/pfaFront"));
